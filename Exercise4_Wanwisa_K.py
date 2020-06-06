dd47dab71764094274125e649386f9b44a268125
PLatonGTY <66166773+PLatonGTY@users.noreply.github.com>
2020-06-06 16:39:41 +0700

    Add files via upload

---
+++ Exercise4_Wanwisa_K.py
@@ -0,0 +1,9 @@
+fe = 100.00
+GB = 90.00
+ITCS = 100.00
+Com = 100.00
+
+print('Foundation English :', fe)
+print('General Business : ', GB)
+print('Introduction to Computer Systems : ', ITCS)
+print('Computer Programming :', Com)
