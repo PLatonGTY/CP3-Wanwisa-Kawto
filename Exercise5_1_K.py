3a8c46065c45d3dbc37abe3dc57480636c102e94
PLatonGTY <66166773+PLatonGTY@users.noreply.github.com>
2020-06-10 20:10:19 +0700

    Add files via upload

---
+++ Exercise5_1_K.py
@@ -0,0 +1,7 @@
+x = int(input("Enter the first number : "))
+y = int(input("Enter the secound number :"))
+
+print(x,"+",y,"=",x+y)
+print(x,"-",y,"=",x-y)
+print(x,"*",y,"=",x*y)
+print(x,"/",y,"=",int(x/y))
---
+++ Exercise5_2_K.py
@@ -0,0 +1,9 @@
+'''
+โปรแกรมคำนวณหาอัตตราเร็ว
+'''
+s = int(input("Enter the distance (m) :"))
+t = int(input("Enter the time (s) : "))
+v = s/t
+
+print("v =", v ,"m/s")
+
